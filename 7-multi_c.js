da6aaf369540e2ed988d3c5af9e1d9e279cadb27
Magnus Afiawo <afiawomagkatalyst@gmail.com>
2025-11-07 05:30:55 +0000

    variegated looping by providing length in CLI

---
+++ 7-multi_c.js
@@ -0,0 +1,14 @@
+const { argv } = require("node:process");
+
+const args = argv.slice(2);
+const num = Number(args[0]);
+
+if (isNaN(num)) {
+  console.log("Missing number of occurrences");
+} else {
+  let i = 0;
+  do {
+    console.log("C is fun");
+    i++;
+  } while (i < num);
+}
