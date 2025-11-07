1cd72b3906743e62b5f3949fe92d59055f1797f0
Magnus Afiawo <afiawomagkatalyst@gmail.com>
2025-11-07 04:54:00 +0000

    changing string to integer in CLI

---
+++ 5-to_integer.js
@@ -0,0 +1,11 @@
+const { argv } = require("node:process");
+
+const args = argv.slice(2);
+
+const num = Number(args[0]);
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log("Not a number");
+}
