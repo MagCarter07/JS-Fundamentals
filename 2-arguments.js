c24e64a26e8b77c6f640d898a19cf1111a4dfb7c
Magnus Afiawo <afiawomagkatalyst@gmail.com>
2025-11-07 04:27:26 +0000

    working with argv process

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+const { argv } = require("node:process");
+
+const args = argv.slice(2);
+const count = args.length;
+
+if (count === 0) {
+  console.log("No argument");
+} else if (count === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
